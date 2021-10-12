2a8573421197dbfaf57f2d4b48aed1255a7a519e
Sebastien Nicolet <snicolet95@gmail.com>
2021-10-12 17:23:49 +0200

    added async context manager to swap the client to use

--- motorized/client.py
+++ motorized/client.py
@@ -1,5 +1,7 @@
+from typing import Optional
 
 from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from contextlib import asynccontextmanager
 
 
 class Connection:
@@ -14,6 +16,19 @@ class Connection:
     async def disconnect(self) -> None:
         self.client = None
         self.database = None
+
+
+@asynccontextmanager
+async def use_client(*args, client: Optional[AsyncIOMotorClient] = None, **kwargs):
+    if not client:
+        client = AsyncIOMotorClient(*args, **kwargs)
+    old_client = connection.client
+    old_database = connection.database
+    connection.client = client
+    connection.database = client.get_default_database()
+    yield
+    connection.client = old_client
+    connection.database = old_database
 
 
 connection = Connection()
