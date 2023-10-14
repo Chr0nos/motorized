bd729fa1bb68ea9c6a6d5c3bfd9d457c0cbafdad
Sebastien Nicolet <snicolet95@gmail.com>
2023-10-14 17:21:57 +0200

    updated animals example

--- examples/animals/models.py
+++ examples/animals/models.py
@@ -1,13 +1,24 @@
-from motorized import Document, Field
-from typing import List, Optional
 from datetime import datetime
 
+from pydantic import BaseModel
+from pydantic_partial import PartialModelMixin
 
-class Animal(Document):
+from motorized import Document, Field
+
+
+# the writer class is the one that the user can interract with.
+class AnimalWriter(PartialModelMixin, BaseModel):
     name: str
-    legs: int = Field(default=4, ge=0, lt=5)
-    is_god: bool = Field(default=False, read_only=True)
-    tags: Optional[List[str]] = None
-    # this will not be exposed trought the api since the field is private
-    internal_comment: Optional[str] = Field(default=None, private=True)
-    created: datetime = Field(default_factory=datetime.utcnow, read_only=True)
+    legs: int = Field(ge=0, lt=5)
+
+
+# the reader class is the one with what the user can read.
+class AnimalReader(Document, AnimalWriter):
+    tags: list[str] | None = None
+    created: datetime = Field(default_factory=datetime.utcnow)
+
+
+# this one container read + write + all private attributes
+class Animal(AnimalReader):
+    internal_comment: str | None = Field(default=None)
+    is_god: bool = Field(default=False)
